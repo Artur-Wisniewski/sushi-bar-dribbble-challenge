689d6fcf854b33b850ccc88555a549247ccffb91
SweetCherry <artwis1998@gmail.com>
2024-01-21 17:28:11 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/dev/wisniewsky/sushi_bar_challenge/dribbble_sushi_bar_challenge/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.wisniewsky.sushi_bar_challenge.dribbble_sushi_bar_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/starter_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class StarterApp extends StatelessWidget {
+  const StarterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
